232a23bd4181d0a3a696a82ed1036de4cb8b44ef
Amita  chorghe <67407962+amitachorghe@users.noreply.github.com>
2022-01-23 20:18:40 +0530

    Add files via upload

---
+++ kfrequentwords.java
@@ -0,0 +1,84 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.lang.Math;
+
+import java.util.*;
+
+class kfrequentwords {
+    public List<String> topKFrequentAlternate(final String[] words, int k) {
+        final Map<String, Integer> freq = new HashMap<Integer>();
+
+        final Queue<WordFreq> queue = new PriorityQueue<>((w1, w2) -> {
+            if (w1.getFreq() != w2.getFreq()) {
+                return w1.getFreq() - w2.getFreq();
+            }
+            return w2.getWord().compareTo(w1.getWord());
+        });
+
+        final List<String> result = new ArrayList<>();
+
+        for (final String word : words) {
+            if (freq.containsKey(word)) {
+                final int count = freq.get(word);
+                freq.put(word, count + 1);
+            } else {
+                freq.put(word, 1);
+            }
+        }
+
+        for (final Map.Entry<String, Integer> entry : freq.entrySet()) {
+            queue.offer(new WordFreq(entry.getKey(), entry.getValue()));
+
+            if (queue.size() > k) {
+                queue.poll();
+            }
+        }
+
+        while (k-- > 0) {
+            result.add(queue.poll().getWord());
+        }
+
+        Collections.reverse(result);
+        return result;
+    }
+}
+
+class WordFreq {
+    private final String word;
+    private final int freq;
+
+    WordFreq(final String word, final int freq) {
+        this.word = word;
+        this.freq = freq;
+    }
+
+    String getWord() {
+        return this.word;
+    }
+
+    int getFreq() {
+        return this.freq;
+    }
+
+    @Override
+    public String toString() {
+        return Objects.toString(word) + "->" + Objects.toString(freq);
+    }
+
+    public static void main(String[] args) {
+        kfrequentwords t = new kfrequentwords();
+        String[] words = { "amita", "sakshi", "pandu", "nanu" };
+        int k = 2;
+        List<String> res = new ArrayList<String>();
+        res = t.topKFrequentAlternate(words, k);
+        ListIterator<String> lItr = res.listIterator();
+        while (lItr.hasNext()) {
+            System.out.println(lItr.next());
+        }
+    }
+}
